ed024f1c88e9745fec68e4c765f3b48b9a8a8a6d
Martyn Rees <martyn@supercube.co.uk>
2026-03-29 14:57:46 +0100

    small refactor merging 2 functions

--- src/index-ship.ts
+++ src/index-ship.ts
@@ -3,14 +3,9 @@ import Gun from "./modules/gun.ts";
 import { shipSpecs, gunSpec } from "./gamedata.js";
 import { changeGameState } from "./state/gameState.ts";
 
-function initShip(pos: { x: number; y: number }): Ship {
+export function addNewShip(pos: { x: number; y: number }) {
   const ship = new Ship(pos, "ship", shipSpecs);
   const gun = new Gun(gunSpec);
   ship.attachGun(gun);
-  return ship;
-}
-
-export function addNewShip(pos: { x: number; y: number }) {
-  const ship: Ship = initShip(pos);
   changeGameState({ action: "add ship", gameElement: ship });
 }
