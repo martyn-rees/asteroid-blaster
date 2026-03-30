c176c7ffc84497e9c00b693a514dc2f6ed141a32
Martyn Rees <martyn@supercube.co.uk>
2026-03-30 18:03:33 +0100

    events moved to their own file

---
+++ src/actions/events.ts
@@ -0,0 +1,83 @@
+import { changeGameState } from "../state/gameState";
+import { createButton } from "../ui/button";
+import { addToScreen, removeFromScreen } from "../render/render";
+import { addNewShip } from "../index-ship";
+import { addNewRocksForNewLevel } from "../index-rock";
+import { gameScreen } from "../index";
+
+/* ADD BUTTONS AND BUTTON HANDLERS */
+function addStartButton() {
+  const startButton = createButton({
+    label: "start",
+    id: "startButton",
+    className: "start-button",
+    onClick: () => changeGameState({ action: "state", gameElement: "playing" }),
+  });
+  addToScreen(startButton, gameScreen.id);
+}
+
+function addPauseButton() {
+  const pauseButton = createButton({
+    label: "pause",
+    id: "pauseButton",
+    className: "pause-button",
+    onClick: () => changeGameState({ action: "state", gameElement: "paused" }),
+  });
+  addToScreen(pauseButton, gameScreen.id);
+}
+
+function addResumeButton() {
+  const resumeButton = createButton({
+    label: "resume",
+    id: "resumeButton",
+    className: "pause-button",
+    onClick: () => changeGameState({ action: "state", gameElement: "playing" }),
+  });
+  addToScreen(resumeButton, gameScreen.id);
+}
+
+// add ship and then add rocks after a set time and set score to 0
+function setUpLevel() {
+  changeGameState({ action: "score", gameElement: 0 });
+  addNewShip(gameScreen.screenCentre);
+  setTimeout(
+    () =>
+      addNewRocksForNewLevel({
+        rockAmount: 8,
+        screenSize: gameScreen.screenSize,
+      }),
+    1000,
+  );
+}
+
+function onEnter(screen: string) {
+  switch (screen) {
+    case "start":
+      changeGameState({ action: "state", gameElement: "start" });
+      addStartButton();
+      changeGameState({ action: "state", gameElement: "start" });
+      break;
+    case "playing":
+      changeGameState({ action: "state", gameElement: "playing" });
+      addPauseButton();
+      break;
+    case "pause":
+      changeGameState({ action: "state", gameElement: "paused" });
+      removeFromScreen("pauseButton");
+      addResumeButton();
+      break;
+  }
+}
+
+function onExit(screen: string) {
+  switch (screen) {
+    case "start":
+      removeFromScreen("startButton");
+      break;
+    case "pause":
+      removeFromScreen("resumeButton");
+      break;
+  }
+}
+
+export { onEnter, onExit, setUpLevel };
--- src/ui/button.ts
+++ src/ui/button.ts
@@ -2,24 +2,18 @@ export function createButton({
   label,
   id,
   className,
-  buttonCallback,
+  onClick,
 }: {
   label: string;
   id: string;
   className: string;
-  buttonCallback: Function;
+  onClick: Function;
 }): HTMLElement {
   // eg. <button id="startButton" class="start-button press-start-2p-regular">start</button>
   let button = document.createElement("button");
   button.textContent = label;
   button.setAttribute("id", id);
   button.setAttribute("class", `${className} press-start-2p-regular`);
-  button.addEventListener(
-    "click",
-    function () {
-      buttonCallback();
-    },
-    { once: true },
-  );
+  button.addEventListener("click", () => onClick(), { once: true });
   return button;
 }
